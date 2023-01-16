220341278c2afd21bdfd2c56319eaf75143f0eb4
Kardelen Kamsız <kardelen_kamsiz@outlook.com>
2023-01-16 21:27:00 +0300

    CountDownTimer
    10 dan geriye doğru sayım yapar.

---
+++ KotlinCountDownTimer/app/src/main/java/com/kamsiz/kotlincountdowntimer/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.kamsiz.kotlincountdowntimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Abstract-Inheritance
+
+        }
+    }
